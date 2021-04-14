89057d04404108a0477834a4c7d39833f368a11f
JavieraTM <javieratm13@gmail.com>
2021-04-14 00:57:02 -0400

    shopcart y modales

--- src/lib/view/templateShopcart.js
+++ src/lib/view/templateShopcart.js
@@ -2,47 +2,81 @@ export const shopcart = () => {
   const divShopcart = document.createElement('div');
 
   const viewShopcart = `
-  <div class='shopcart' id='shopcart-screen'>
-  
-    <div class="shopcart-header">
-      <img src="img/logocm-white.png" alt="">
-      <h2 class="partner-name">Hola, Gabriela Morales</h2>
-    </div>
+  <div class="shopcart" id="shopcart-screen">
+  <div class="shopcart-header">
+    <img src="img/logocm-white.png" alt="" />
+    <h2 class="partner-name">Hola, Gabriela Morales</h2>
+  </div>
 
-    <div>
-      <ul class="items">
-        <li>Producto</li>
-        <li>Cantidad</li>
-        <li>Precio</li>
-      </ul>
-      <div class="">
+  <div>
+    <ul class="items">
+      <li>Producto</li>
+      <li>Cantidad</li>
+      <li>Precio</li>
+    </ul>
+    <div class="">
+      <div>
         <button class="the-button" id="theButton">+</button>
-        <div>
-          <p id="itemsContainer" class='product-container'></p>
-        </div>
+        <p id="itemsContainer"></p>
+      </div>
+      <div>
         <button class="the-button" id="theButton2">+</button>
-        <div>
-          <p id="itemsContainer2" class='product-container'></p>
-        </div>
+        <p id="itemsContainer2"></p>
+      </div>
+      <div>
         <button class="the-button" id="theButton3">+</button>
-        <div>
-          <p id="itemsContainer3" class='product-container'></p>
-        </div>
+        <p id="itemsContainer3"></p>
+      </div>
+      <div>
         <button class="the-button" id="theButton4">+</button>
-        <div>
-          <p id="itemsContainer4" class='product-container'></p>
-        </div>
-        <div>
-        <p>Total:</p>
-        <p id="precioTotal"></p>
-        </div>
+        <p id="itemsContainer4"></p>
       </div>
 
+      <div class="footer">
+        <div class="cancelPurchase">
+          <a href='' class="cancel-purchase">Cancelar compra</a>
+        </div>
+        
+        <div class="right">
+          <div>
+            <div class="totalPrice">
+              <p class="total">Total:</p>
+              <p class="total-price" id="totalPrice"></p>
+              </br>
+            </div>
+            <p class="empty-cart">Vaciar carrito</p>
+            </br>
+            <button class="finish-shop-button" id='modalShopcartButton'>Finalizar pedido</button>
+          </div>
+        </div>
+      </div>
     </div>
-
   </div>
+  
+  <div class="finish-purchase-modal-background">
+  <div class="finish-purchase-modal">
+    <p class='finish-purchase-text-modal'>Por favor, regresa el lector de código a su lugar para continuar con el pago.</p>
+    <button class='finish-purchase-modal-button' id='closeModalShopcartButton'></button>
+  </div>
+</div>
+
+</div>
+
       `;
   divShopcart.innerHTML = viewShopcart;
+
+  // -------------abrir modal --------------------
+  const modalButton = divShopcart.querySelector('#modalShopcartButton');
+  const modalBackground = divShopcart.querySelector('.finish-purchase-modal-background');
+  const modalClose = divShopcart.querySelector('#closeModalShopcartButton');
+
+  modalButton.addEventListener('click', function () {
+    modalBackground.classList.add('finish-purchase-background-active');
+  });
+
+  modalClose.addEventListener('click', function () {
+    modalBackground.classList.remove('finish-purchase-background-active');
+  });
 
   // Declaramos la data que usaremos
   const items = [
@@ -58,10 +92,6 @@ export const shopcart = () => {
   const button2 = divShopcart.querySelector('#theButton2');
   const button3 = divShopcart.querySelector('#theButton3');
   const button4 = divShopcart.querySelector('#theButton4');
-  const itemsContainer = divShopcart.querySelector('#itemsContainer');
-  const itemsContainer2 = divShopcart.querySelector('#itemsContainer2');
-  const itemsContainer3 = divShopcart.querySelector('#itemsContainer3');
-  const itemsContainer4 = divShopcart.querySelector('#itemsContainer4');
 
   // Con el método map sumamos el valor del precio de los productos
   let total = 0;
@@ -71,41 +101,77 @@ export const shopcart = () => {
   // Le agregamos el evento a los botones para que nos pinten la info de la data
   const addItem = button.addEventListener('click', () => {
     divShopcart.querySelector('#itemsContainer').innerHTML = `
-    <span>${items[0].producto}</span>
-    <span>${items[0].cantidad}</span>
-    <span class='price'>$${items[0].precio}</span>
-    <img src="img/trash.png" alt="trash button" type='button'>
+    <div class='item-container'>
+      <div class='product-container'>
+        <img class='product-img' src="img/product1.png" alt="">
+        <span class='product'>${items[0].producto}</span>
+        <span class='cuantity'>${items[0].cantidad}</span>
+        <span class='price'>$${items[0].precio}</span>
+      </div>
+      <div class='trash'>
+        <img src="img/trash.png" alt="trash button" type='button'>
+      </div>
+      
+    </div>
     `;
   });
 
   const addItem2 = button2.addEventListener('click', () => {
     divShopcart.querySelector('#itemsContainer2').innerHTML = `
-    <span>${items[1].producto}</span>
-    <span>${items[1].cantidad}</span>
-    <span class='price'>$${items[1].precio}</span>
-    <img src="img/trash.png" alt="trash button" type='button'>
+    <div class='item-container'>
+      <div class='product-container'>
+        <img src="img/product2.png" alt="">
+        <span class='product'>${items[1].producto}</span>
+        <span class='cuantity'>${items[1].cantidad}</span>
+        <span class='price'>$${items[1].precio}</span><br>
+      </div>
+      <div class='trash'>
+        <img src="img/trash.png" alt="trash button" type='button'>
+      </div>
+    </div>
     `;
   });
 
   const addItem3 = button3.addEventListener('click', () => {
     divShopcart.querySelector('#itemsContainer3').innerHTML = `
-    <span>${items[2].producto}</span>
-    <span>${items[2].cantidad}</span>
-    <span class='price'>$${items[2].precio}</span>
-    <img src="img/trash.png" alt="trash button" type='button'>
+    <div class='item-container'>
+      <div class='product-container'>
+        <img src="img/product3.png" alt="">
+        <span class='product'>${items[2].producto}</span>
+        <span class='cuantity'>${items[2].cantidad}</span>
+        <span class='price'>$${items[2].precio}</span><br>
+      </div>
+      <div class='trash'>
+        <img src="img/trash.png" alt="trash button" type='button'>
+      </div>
+    </div>
     `;
   });
 
   const addItem4 = button4.addEventListener('click', () => {
     divShopcart.querySelector('#itemsContainer4').innerHTML = `
-    <span>${items[3].producto}</span>
-    <span>${items[3].cantidad}</span>
-    <span class='price'>$${items[3].precio}</span>
-    <img src="img/trash.png" alt="trash button" type='button'>
+    <div class='item-container'>
+      <div class='product-container'>
+        <img src="img/product4.png" alt="">
+        <span class='product'>${items[3].producto}</span>
+        <span class='cuantity'>${items[3].cantidad}</span>
+        <span class='price'>$${items[3].precio}</span><br>
+      </div>
+      <div class='trash'>
+        <img src="img/trash.png" alt="trash button" type='button'>
+      </div>
+    </div>
     `;
 
     // Aquí pintamos el total de la suma del valor de los productos que hicimos con el método map
-    divShopcart.querySelector('#precioTotal').innerHTML = '$' + total;
+    divShopcart.querySelector('#totalPrice').innerHTML = '$' + total;
   });
+
+  // -------------Cambio a template Pay--------------------
+  const goToPay = divShopcart.querySelector('#closeModalShopcartButton');
+  goToPay.addEventListener('click', () => {
+    location.assign('#pay');
+  });
+
   return divShopcart;
 };
